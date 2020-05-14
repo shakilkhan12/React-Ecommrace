cb9b32b2496dcfe19527e4d36304641054d63f64
shakilkhan12 <shakilkhan621@gmail.com>
2020-05-13 21:54:08 -0700

    b

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,11 @@ import React from "react"
 const Banner = () => {
     return(
         <header style={{marginTop: '70px'}}>
-            <div>
+         <div className="headerText">
+         <div>
          <h1><span>AliExpress</span> Ecomrace website</h1>
          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+         </div>
          </div>
         </header>
     )
