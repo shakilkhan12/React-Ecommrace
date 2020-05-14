20fbed2e754c833018d70f68ddab5da27898f054
shakilkhan12 <shakilkhan621@gmail.com>
2020-05-13 21:07:48 -0700

    g

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({cartToggle}) => {
      </form></li> */}
  </ul>
  <ul className="right">
-    <li onClick={cartToggle}><Link to="/cart"><span className="dollor"><i className="fas fa-cart-plus"></i></span><span className="shoppingCartTotal">{shoppingCart.length}</span></Link></li>
+    <li onClick={cartToggle}><Link to="/cart"><span className="dollor"><i className="fas fa-cart-plus"></i></span><span className="shoppingCartTotal">{shoppingCart ? shoppingCart.length : 0}</span></Link></li>
  </ul>
 </nav>
     )
