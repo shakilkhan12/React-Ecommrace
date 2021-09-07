91c05bff6f0e15537e39f612437538780339e578
Shakil Khan <shakilkhan621@gmail.com>
2021-09-07 16:21:23 +0500

    axios package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import './App.css';
+import axios from "axios"
 import Navbar from "./components/Navbar"
 import Products from "./components/Products"
 import Cart from "./components/Cart"
@@ -8,7 +9,13 @@ import ProductContextProvider from "./Global/productContext"
 import CartContextProvider from "./Global/cartContext"
 
 function App() {
-
+  const fetchUsers = async () => {
+    const {data} = await axios.get("/.netlify/functions/users");
+    console.log('Your users: ', data);
+  }
+ useEffect(() => {
+   fetchUsers();
+ }, [])
   return (
     <div>
       <ProductContextProvider>
