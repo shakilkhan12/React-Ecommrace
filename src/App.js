e810cd961ed1c3f033045b1e031a8f2c98d1c5bb
Shakil Khan <shakilkhan621@gmail.com>
2021-09-10 20:11:28 +0500

    sfsd

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import React, {useEffect} from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import axios from "axios"
-import Navbar from "./components/Navbar"
-import Products from "./components/Products"
-import Cart from "./components/Cart"
-import ProductContextProvider from "./Global/productContext"
-import CartContextProvider from "./Global/cartContext"
+import axios from 'axios';
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import Cart from './components/Cart';
+import ProductContextProvider from './Global/productContext';
+import CartContextProvider from './Global/cartContext';
 
 function App() {
-  const fetchUsers = async () => {
-     try {
-      const {data} = await axios.get("/.netlify/functions/users");
-      console.log('Your users: ', data);
-     } catch (error) {
-       console.log('your errors: ', error)
-     }
-  }
- useEffect(() => {
-   fetchUsers();
- }, [])
- console.log('Hello app component!')
-  return (
-    <div>
-      <ProductContextProvider>
-      <CartContextProvider>
-      <Router>
-      <Navbar />
-        <Switch>
-          <Route path="/" exact component={Products} />
-          <Route path="/cart" exact component={Cart} />
-        </Switch>
-      </Router>
-      </CartContextProvider>
-      </ProductContextProvider>  
-    </div>
-  );
+	const fetchUsers = async () => {
+		try {
+			const res = await fetch('/.netlify/functions/users');
+			const data = await res.json();
+			console.log('res: ', data);
+		} catch (error) {
+			console.log('your errors: ', error);
+		}
+	};
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+	console.log('Hello app component!');
+	return (
+		<div>
+			<ProductContextProvider>
+				<CartContextProvider>
+					<Router>
+						<Navbar />
+						<Switch>
+							<Route path='/' exact component={Products} />
+							<Route path='/cart' exact component={Cart} />
+						</Switch>
+					</Router>
+				</CartContextProvider>
+			</ProductContextProvider>
+		</div>
+	);
 }
 
 export default App;
