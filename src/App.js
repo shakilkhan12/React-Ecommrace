95bbd3fda17c397575ecf214020fd60bfc8ff603
Shakil Khan <shakilkhan621@gmail.com>
2021-09-10 20:19:54 +0500

    hello

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 			const data = await res.json();
 			console.log('res: ', data);
 		} catch (error) {
-			console.log('your errors: ', error);
+			console.log('your errors: ', error.data.response);
 		}
 	};
 	useEffect(() => {
